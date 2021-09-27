90960281cbff603da79cd7ff23feb0c26b337868
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2021-09-26 21:31:49 -0400

    game loads without crashing

--- src/main/java/tfar/fishbeatgameforme/ExampleMod.java
+++ src/main/java/tfar/fishbeatgameforme/ExampleMod.java
@@ -6,6 +6,9 @@ import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.commands.CommandSourceStack;
 
 public class ExampleMod implements ModInitializer, CommandRegistrationCallback {
+
+	public static final String MODID = "fishbeatgameforme";
+
 	@Override
 	public void onInitialize() {
 		CommandRegistrationCallback.EVENT.register(this);
--- src/main/java/tfar/fishbeatgameforme/StartCommand.java
+++ src/main/java/tfar/fishbeatgameforme/StartCommand.java
@@ -1,11 +1,19 @@
 package tfar.fishbeatgameforme;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
 
 public class StartCommand {
 
     public static void register(CommandDispatcher<CommandSourceStack> commandDispatcher) {
-        
+        commandDispatcher.register(Commands.literal(ExampleMod.MODID)
+                .then(Commands.literal("start")).executes(StartCommand::start)
+        );
+    }
+
+    private static int start(CommandContext<CommandSourceStack> context) {
+        return 1;
     }
 }
